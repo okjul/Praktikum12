9dedbb7b79b503a972de343daf0784bcf2ed871b
okjul <103446510+okjul@users.noreply.github.com>
2022-07-14 00:44:09 +0700

    Add files via upload

---
+++ app/src/main/java/com/oktavia_19102254/praktikum12/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oktavia_19102254.praktikum12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
